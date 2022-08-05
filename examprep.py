470f522bf4b415e6d33df1bc65dd39b2a6090381
Pravin Se <pvs1516@gmail.comm>
2022-08-05 13:09:02 +0100

    Update 1

---
+++ examprep.py
@@ -0,0 +1,43 @@
+# var= "James Bond"
+# print(var[2::-1])
+#
+# {"apple,"banana","cherry"}
+
+x = int(10)
+b = float(12.8)
+c = float(5.6)
+
+print(x + b + c)
+
+class Juice:
+    def __init__(self):
+        self.sweet = True
+        self.sour = True
+
+
+    def cool(self):
+        return "ice cold juice tastes amazing"
+
+
+
+juice_object = Juice()
+
+print(juice_object.cool())
+
+num = int(input("Enter a number: "))   # The % sign is like division only it checks for the remainder, so if the number divided by 2 has a remainder of 0 it's even otherwise odd.
+if num % 2 == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+character_name = "Jonty "
+character_fav = "Run"
+print(character_name + "likes to go shopping")
+print("Andy is 657 years old")
+print("In the evenings, Andy likes to go for a walk")
+
+
+
+
+
+
---
+++ orange.py
@@ -0,0 +1,15 @@
+from examprep import Juice
+
+class Orange:
+    def __init__(self):
+        super().__init__()
+
+
+        
+    def vitc(self):
+        return "very healthy for you"
+
+
+orange_object = Orange()
+
+print(orange_object.vitc())
