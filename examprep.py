d2731d28f2cadc074c9821c86c173951f55d6c1b
Pravin Se <pvs1516@gmail.comm>
2022-08-05 16:01:41 +0100

    Update 4

--- examprep.py
+++ examprep.py
@@ -67,7 +67,42 @@ is_male = True
 is_tall = True
 
 if is_male or is_tall:
-    print("You are a male or tall or both ")
+    print("You are a tall male ")
+elif is_male and not(is_tall):
+    print("You are a short male ")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
 
 else:
-    print("You are neither male nor tall ")
+    print("You are not a male and not tall ")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print (max_num(3, 4, 5))
+
+
+num1 = float(input("Enter a number: "))
+operator = input("Enter operator: ")
+num3 = float(input("Enter a second number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "*":
+    print(num1 * num2)
+else:
+    print("invalid")
+
+
+
